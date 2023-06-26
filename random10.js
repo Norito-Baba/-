f755868c7254d453c86bf1a61de35228e18e992e
Norito-Baba <137675974+Norito-Baba@users.noreply.github.com>
2023-06-26 18:34:54 +0900

    random10で問題の表示と正誤確認に成功

--- random10.js
+++ random10.js
@@ -1,6 +1,7 @@
 var c=0,i=0;
+var AllQandA,num;
 function inanswer(){
-    if (answer.value==AllQandA[num.answer]){
+    if (answer.value==AllQandA[num].answer){
         c+=1;
         CorI.innerText="正解"
     }
@@ -10,11 +11,25 @@ function inanswer(){
     }
 }
 
-AllQandA=JSON.parse(fs.readFileSync(data.json));
-num = Math.floor(Math.random()*AllQandA.length)
-document.getElementById("question")=AllQandA[num.question];
 
-let answer=document.getElementById("answer");
-let CorI=document.getElementById("CorI");
-let answerbutton=document.getElementById("answerbutton")
-answerbutton.onclick=inanswer;
+
+
+window.onload=function(){
+    var obj=new XMLHttpRequest();
+    obj.open("get","./data.json");
+    obj.send();
+    obj.onreadystatechange=function(){
+	    if(obj.readyState==4&&obj.status==200){
+            AllQandA=JSON.parse(obj.response);
+            var value=Object.keys(AllQandA);
+            num = Math.floor(Math.random()*value.length);
+            question=document.getElementById("question");
+            question.innerText=AllQandA[num].question;
+            let answer=document.getElementById("answer");
+            let CorI=document.getElementById("CorI");
+            let answerbutton=document.getElementById("answerbutton")
+            answerbutton.onclick=inanswer;
+            return console.log(AllQandA);
+        }
+    };
+}
--- toppage.js
+++ toppage.js
@@ -1,11 +1,12 @@
 function randomClick() {
-    location.href="random10.html"
+    location.href="./random10.html"
 }
 function endlessClick(){
-    location.href="endless.html"
+    location.href="./endless.html"
 }
-
-let button1 = document.getElementById('random10');
-button1.onclick=randomClick;
-let button2 = document.getElementById('endless');
-button2.onclick=endlessClick;
+window.onload=function(){
+    let button1 = document.getElementById('random10');
+    button1.onclick=randomClick;
+    let button2 = document.getElementById('endless');
+    button2.onclick=endlessClick;
+}
